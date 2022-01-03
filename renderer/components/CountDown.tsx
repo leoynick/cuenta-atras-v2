eb7cbbe621be73300143a39b9674f97b9347d9ac
Yannick Miñano <leoynick@gmail.com>
2022-01-03 15:48:33 +0100

    - Añadir build para Mac. - Ajustar tamaño de cuentra atrás.

--- renderer/components/CountDown.tsx
+++ renderer/components/CountDown.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactCountdownClock from "react-countdown-clock";
 
-const CountDown = ({ seconds }) => {
-  //   const date = new Date(2020, 10, 10, 20, 0, 0, 0);
-  //   const seconds = Math.abs(Math.floor((date.getTime() - Date.now()) / 1000));
-
+const CountDown = ({ seconds, onComplete }) => {
   return (
     <ReactCountdownClock
       seconds={seconds}
       color="#a854f7"
       alpha={0.9}
-      size={150}
-      weight={5}
+      size={200}
+      weight={8}
       fontSize="30px"
-      //   onComplete={myCallback}
+      onComplete={onComplete}
     />
   );
 };
--- renderer/pages/meeting/[day].tsx
+++ renderer/pages/meeting/[day].tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ArrowCircleLeftIcon } from "@heroicons/react/solid";
@@ -7,21 +8,22 @@ import CountDown from "../../components/CountDown";
 import RememberCard from "../../components/RememberCard";
 
 function Meeting() {
+  const [isComplete, setIsComplete] = useState(false);
   const { query } = useRouter();
   const { sec } = query;
-  const isWeekend = false;
+  const isWeekend = true;
 
   return (
     <div className="bg-[url('/images/background.png')] flex min-h-screen bg-no-repeat bg-cover justify-center">
-      <div className="flex items-center flex-col justify-center">
-        <h2 className="text-3xl text-purple-500">
-          <div className="absolute left-5 top-5">
-            <Link href="/home">
-              <div className="w-5 h-2">
-                <ArrowCircleLeftIcon className="text-purple-500 w-10" />
-              </div>
-            </Link>
+      <div className="absolute left-10 top-5">
+        <Link href="/home">
+          <div className="w-5 h-2">
+            <ArrowCircleLeftIcon className="text-purple-500 w-10" />
           </div>
+        </Link>
+      </div>
+      <div className="flex items-center flex-col justify-center">
+        <h2 className="text-3xl text-purple-500 relative text-center">
           “A los que buscan a Jehová no les faltará nada bueno”<br></br>
           (Salmos 34:10)
         </h2>
@@ -32,17 +34,25 @@ function Meeting() {
             loop={true}
           />
         </div>
-        <div className="flex items-center gap-5 my-12">
-          <div className="bg-purple-500 rounded-md text-white flex gap-4 py-5 px-3 items-center w-64 text-center">
-            <h3 className="font-semibold text-lg">
-              La reunión de fin de semana comienza en:
-            </h3>
-          </div>
-          <CountDown seconds={sec} />
+        <div className="h-35 my-20">
+          {isComplete ? (
+            <span className="font-semibold text-3xl text-purple-500 flex items-center">
+              ¡EMPEZAMOS
+            </span>
+          ) : (
+            <div className="flex items-center gap-24 ">
+              <div className="bg-purple-500 rounded-md text-white flex gap-4 py-5 px-3 items-center w-64 text-center">
+                <h3 className="font-semibold text-xl">
+                  La reunión de fin de semana comienza en:
+                </h3>
+              </div>
+              <CountDown seconds={sec} onComplete={() => setIsComplete(true)} />
+            </div>
+          )}
         </div>
-        <div className="mt-7 flex flex-col items-center">
+        <div className="mt-7 flex flex-col items-center gap-4">
           <h3 className="text-xl font-bold">RECORDATORIOS</h3>
-          <div className="flex gap-2">
+          <div className="flex gap-4">
             <RememberCard
               text="No se permite tomar fotos o videos durante la reunión."
               image="photo"
